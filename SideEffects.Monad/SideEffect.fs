60127b7723ffc225ae95f1630e162a19cca5cae2
Tommi Lilja <tommi.lilja@icloud.com>
2023-05-07 00:18:10 +0200

    Add ret

--- SideEffects.Monad/SideEffect.fs
+++ SideEffects.Monad/SideEffect.fs
@@ -8,21 +8,23 @@ type SideEffectAsync<'a> = 'a Async SideEffect
 
 module SideEffect =
     
+    let ret = Pure
+    
     let rec bind fn = function
         | Free i -> Free (Instruction.map (bind fn) i)
         | Pure x -> fn x
     
-    let map fn = bind (fn >> Pure)
+    let map fn = bind (fn >> ret)
     
     let rec handle interpreter = function
         | Free i -> Instruction.peel interpreter i |> handle interpreter
         | Pure x -> x   
 
-    let log str = Free (Log (str, Pure))
+    let log str = Free (Log (str, ret))
     
-    let createGuid () = Free (CreateGuid Pure)
+    let createGuid () = Free (CreateGuid ret)
     
-    let getTime () = Free (GetTime Pure)
+    let getTime () = Free (GetTime ret)
     
-    let httpRequest url : SideEffectAsync<_> = Free (HttpRequest (url, Pure))
+    let httpRequest url = Free (HttpRequest (url, ret))
     
--- SideEffects.Monad/SideEffectAsync.fs
+++ SideEffects.Monad/SideEffectAsync.fs
@@ -2,6 +2,8 @@ namespace SideEffects.Monad
 
 module SideEffectAsync =
 
+    let ret x = SideEffect.ret (Async.ret x)
+    
     let bind fn = SideEffect.map (Async.bind fn)
     
     let map fn = bind (fn >> Async.ret)
