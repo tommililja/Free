83dd05d7bae5eb830da5e266553acd2d110d3a51
Tommi Lilja <tommi.lilja@icloud.com>
2023-05-16 21:22:51 +0200

    Rename

--- SideEffects.Monad/SideEffectAsync.fs
+++ SideEffects.Monad/SideEffectAsync.fs
@@ -6,7 +6,7 @@ module SideEffectAsync =
 
     let ret x = SideEffect.ret (Async.ret x)
     
-    let bind fn = SideEffect.map (Async.bind fn)
+    let bindAsync fn = SideEffect.map (Async.bind fn)
     
-    let map fn = bind (fn >> Async.ret)
+    let map fn = bindAsync (fn >> Async.ret)
     
